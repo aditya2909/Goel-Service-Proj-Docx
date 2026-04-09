49af364f98ee29cb6bbbac9e8f85adbe48e74406
Aditya Jaiswal <86339116+aditya2909@users.noreply.github.com>
2026-04-09 12:47:31 +0530

    Update API base URL for service

--- client/src/service/API.js
+++ client/src/service/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://goel-service-proj-docx-k8fmvuhub-aditya2909s-projects.vercel.app/api/", // change if needed
+  baseURL: "https://goel-service-proj-docx.vercel.app/api/", // change if needed
 });
 
 export default API;
